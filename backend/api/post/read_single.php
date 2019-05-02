ca2e93e4457214bed7864dbcebfbcf382d075ff2
MKA1500 <maciejkabsch@gmail.com>
2019-05-02 22:10:10 +0200

    read single article

--- backend/api/post/read_single.php
+++ backend/api/post/read_single.php
@@ -5,9 +5,20 @@
   include_once '../../config/Database.php';
   include_once '../../models/Post.php';
 
-  // instantiate db & connect it:
   $database = new Database();
   $db = $database->connect();
 
-  // instantiate blog post object:
   $post = new Post($db);
+
+  $post->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+  $post->read_single();
+
+  $post_arr = array(
+    'id' => $post->id,
+    'title' => $post->title,
+    'body' => $post->body,
+    'author' => $post->author,
+    'category_id' => $post->category_id,
+    'category_name' => $post->category_name,
+  );
