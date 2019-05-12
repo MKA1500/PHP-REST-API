f9b627a69a7d45cf5d10f45c12a9c3abe51aa38e
MKA1500 <maciejkabsch@gmail.com>
2019-05-12 11:51:28 +0200

    displaying component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { SingleComponent } from './home/single/single.component';
 import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, children: [
-    { path: ':id', component: SingleComponent }
-  ] },
+  { path: '', component: HomeComponent },
   { path: 'admin', component: AdminComponent, children: [
   //   { path: 'admin', component: ArticlesListComponent },
   //   { path: 'new', component: NewArticleComponent },
